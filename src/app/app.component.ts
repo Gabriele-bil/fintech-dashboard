854a400cb7ce4fefc382f638489e0f1dd16c3fa2
Gabriele-Bil <gabriele.bilello1@gmail.com>
2021-10-31 11:18:46 +0100

    implemented login component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,16 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'ft-root',
-  template: ``,
+  template: `
+    <div class="d-flex align-items-center justify-content-center">
+      <ft-sign-in></ft-sign-in>
+    </div>
+  `,
+  styles: [`
+    div {
+      min-height: 100vh;
+    }
+  `]
 })
 export class AppComponent {
   title = 'fintech-dashboard';
---
+++ src/app/views/sign-in.component.ts
@@ -0,0 +1,66 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ft-sign-in',
+  template: `
+    <mat-card>
+      <form #f="ngForm" (ngSubmit)="logUser(f.value, f.invalid)">
+        <mat-form-field class="w-100 mb-3" appearance="fill">
+          <mat-label>Email</mat-label>
+          <mat-icon matPrefix>person</mat-icon>
+          <input
+            type="email" matInput
+            placeholder="Ex. pat@example.com"
+            [ngModel] name="email" required
+            #email="ngModel" email
+          >
+          <mat-error *ngIf="email.touched && email.errors?.email">Inserire una mail valida</mat-error>
+          <mat-error *ngIf="email.touched && email.errors?.required">Questo campo è obbligatorio</mat-error>
+        </mat-form-field>
+
+        <mat-form-field appearance="fill" class="w-100 mb-3">
+          <mat-label>Password</mat-label>
+          <mat-icon matPrefix>lock</mat-icon>
+          <input
+            matInput
+            [type]="hidePassword ? 'password' : 'text'"
+            [ngModel] #password="ngModel"
+            name="password" minlength="6" required
+          >
+          <button type="button" mat-icon-button matSuffix (click)="hidePassword = !hidePassword">
+            <mat-icon>{{hidePassword ? 'visibility_off' : 'visibility'}}</mat-icon>
+          </button>
+          <mat-error *ngIf="password.touched && password.errors?.minlength">
+            La password deve essere minimo di {{ password.errors?.minlength.requiredLength }} caratteri.
+            Inserire altri {{ password.errors?.minlength.requiredLength - password.errors?.minlength.actualLength }} caratteri
+          </mat-error>
+          <mat-error *ngIf="email.touched && email.errors?.required">Questo campo è obbligatorio</mat-error>
+        </mat-form-field>
+
+        <button
+          mat-raised-button color="primary"
+          class="w-100" type="submit"
+          [disabled]="f.invalid"
+        >Accedi</button>
+        <a mat-button color="accent" class="mt-2 ps-0">Crea un nuovo account</a>
+      </form>
+    </mat-card>
+  `,
+  styles: [`
+    mat-card {
+      max-width: 500px;
+    }
+  `]
+})
+export class SignInComponent {
+  public hidePassword = true;
+  public email: string = '';
+  public password: string = '';
+  constructor() { }
+
+  public logUser(user: { email: string, password: string }, invalid: boolean | null): void {
+    if (!invalid) {
+      console.log(user);
+    }
+  }
+}
