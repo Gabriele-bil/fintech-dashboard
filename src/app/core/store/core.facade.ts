8dddeb8d543481e2d70f56cab95a7e4a0de60377
GabrieleBi <gabriele.bilello1@gmail.com>
2021-12-06 21:50:36 +0100

    fix error spinner

--- src/app/core/store/core.facade.ts
+++ src/app/core/store/core.facade.ts
@@ -4,12 +4,13 @@ import { getLoading$, getTheme$ } from "./core.selectors";
 import { Observable } from "rxjs";
 import { setSpinner, setTheme } from "./core.actions";
 import { Theme } from "./core.reducer";
+import { delay } from "rxjs/operators";
 
 @Injectable({
   providedIn: "root"
 })
 export class CoreFacade {
-  public loading$: Observable<boolean> = this.store.select(getLoading$);
+  public loading$: Observable<boolean> = this.store.select(getLoading$).pipe(delay(0)); // Ho dovuto aggiungere il delay altrimenti mi dava: ExpressionChangedAfterItHasBeenCheckedError
   public theme$: Observable<Theme> = this.store.select(getTheme$);
 
   constructor(private store: Store) {
