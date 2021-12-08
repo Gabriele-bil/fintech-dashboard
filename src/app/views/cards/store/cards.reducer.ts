b004f0b03399a89346160bfa253ce526aacd57fc
GabrieleBi <gabriele.bilello1@gmail.com>
2021-12-08 21:22:09 +0100

    implemented entity in cards store

--- src/app/views/cards/store/cards.reducer.ts
+++ src/app/views/cards/store/cards.reducer.ts
@@ -1,26 +1,21 @@
 import { Card } from "src/app/models/card.model";
 import { createReducer, on } from "@ngrx/store";
 import { addCardSuccess, setCardsSuccess, removeCardSuccess } from "./cards.actions";
+import { createEntityAdapter, EntityState } from "@ngrx/entity";
 
 export const cardsFeatureKey = 'cards';
 
-export interface CardsState {
-  cards: Card[];
-}
+export interface CardsState extends EntityState<Card>{ }
 
-export const initialState: CardsState = {
-  cards: []
-}
+export const cardsAdapter = createEntityAdapter<Card>({
+  selectId: (card) => card._id
+})
+
+export const initialState: CardsState = cardsAdapter.getInitialState();
 
 export const cardsReducer = createReducer(
   initialState,
-  on(setCardsSuccess, (state, action) => ({ ...state, cards: action.cards })),
-  on(removeCardSuccess, (state, action) => ({
-    ...state,
-    cards: state.cards.filter(c => c._id !== action.cardId)
-  })),
-  on(addCardSuccess, (state, action) => ({
-    ...state,
-    cards: [...state.cards, action.card]
-  }))
+  on(setCardsSuccess, (state, { cards }) => cardsAdapter.setAll(cards, state)),
+  on(removeCardSuccess, (state, { cardId }) => cardsAdapter.removeOne(cardId, state)),
+  on(addCardSuccess, (state, { card }) => cardsAdapter.addOne(card, state))
 );
